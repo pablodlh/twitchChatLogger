47a0cc670a314ccaf45eb792ddeda7b1336c790a
Pablo <220241@fppiramide.com>
2023-12-01 12:52:45 +0100

    añadida funcion buscar por usuario

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 import React, { useEffect, useState } from 'react';
 
 interface Mensaje {
@@ -9,38 +10,43 @@ interface Mensaje {
 }
 
 const App = () => {
-  const [todosLosMensajes, setTodosLosMensajes] = useState<Mensaje[]>([]); 
+  const [todosLosMensajes, setTodosLosMensajes] = useState<Mensaje[]>([]);
+  const [mensajesFiltrados, setMensajesFiltrados] = useState<Mensaje[]>([]);
+  const [usuarioFiltrado, setUsuarioFiltrado] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8080/mensajes')
       .then(response => response.json())
-      .then(data => setTodosLosMensajes(data))
+      .then(data => {
+        setTodosLosMensajes(data);
+        setMensajesFiltrados(data);
+      })
   }, [])
+
+  const filtrarPorUsuario = () => {
+    setMensajesFiltrados(todosLosMensajes.filter(mensaje => mensaje.usuario === usuarioFiltrado));
+  }
 
   return (
     <main className='text-white'>
-      <h1 className='text-white text-3xl  text-center '>Mensajes guardados en la base de datos</h1>
+      <h1 className='text-white text-3xl  text-center  '>Mensajes guardados en la base de datos</h1>
       <section className='flex flex-col items-start ml-6'>
-        <form>
-          <label > Filtrar por usuario<input className='text-black' id='filtrarUsuario'></input></label>
-          <button className=' border border-purple-800 w-14 bg-purple-400'>Buscar</button>
-        </form>
-
-           {todosLosMensajes.map((mensaje) => (
-        
-        <div key={mensaje.id} className='text-white flex flex-col mt-3'>
-          <p className='border border-purple-200 w-min rounded-xl ml-1  opacity-80  text-sm '> @{mensaje.usuario} </p>
-          <p className='border  border-purple-400  rounded-2xl text-lg'>{mensaje.contenidoMensaje}</p>
-        </div>
-      ))}
-
-
-
-
-     
-
+          <label > Filtrar por usuario
+            <input 
+              className='text-black' 
+              id='filtrarUsuario'
+              onChange={e => setUsuarioFiltrado(e.target.value)}
+            />
+          </label>
+          <button onClick={filtrarPorUsuario} className=' border border-purple-800 w-14 bg-purple-400'>Buscar</button>
+          {mensajesFiltrados.map((mensaje) => (
+            <div key={mensaje.id} className='text-white flex flex-col mt-3'>
+              <p className='border border-purple-200 w-min rounded-xl ml-1  opacity-80  text-sm '> @{mensaje.usuario} </p>
+              <p className='border  border-purple-400  rounded-2xl text-lg pl-2 pr-2'>{mensaje.contenidoMensaje}</p>
+            </div>
+          ))}
       </section>
-      </main>
+    </main>
   )
 }
 
