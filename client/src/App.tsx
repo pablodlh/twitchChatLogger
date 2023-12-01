901ccc320e5fa786c878e5538ee36a77d33dc517
Pablo <220241@fppiramide.com>
2023-12-01 11:57:53 +0100

    cambio en los estilos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 
 interface Mensaje {
-  id?: string;
+  id: string;
   usuario: string;
   contenidoMensaje: string;
   canal: string;
@@ -18,19 +18,29 @@ const App = () => {
   }, [])
 
   return (
-    <>
-      <h1 className='text-white text-3xl  text-center  '>Mensajes guardados en la base de datos</h1>
+    <main className='text-white'>
+      <h1 className='text-white text-3xl  text-center '>Mensajes guardados en la base de datos</h1>
       <section className='flex flex-col items-start ml-6'>
-      {todosLosMensajes.map((mensaje, index) => (
+        <form>
+          <label > Filtrar por usuario<input className='text-black' id='filtrarUsuario'></input></label>
+          <button className=' border border-purple-800 w-14 bg-purple-400'>Buscar</button>
+        </form>
+
+           {todosLosMensajes.map((mensaje) => (
         
-        <div key={index} className='text-white flex flex-col mt-3'>
+        <div key={mensaje.id} className='text-white flex flex-col mt-3'>
           <p className='border border-purple-200 w-min rounded-xl ml-1  opacity-80  text-sm '> @{mensaje.usuario} </p>
-          <p  className='border  border-purple-400  rounded-2xl text-lg'>{mensaje.contenidoMensaje}</p>
-          
+          <p className='border  border-purple-400  rounded-2xl text-lg'>{mensaje.contenidoMensaje}</p>
         </div>
       ))}
+
+
+
+
+     
+
       </section>
-    </>
+      </main>
   )
 }
 
