b0637cf5cadf3c065af674e4d3e3faa3cce55369
pablodlh <220241@fppiramide.com>
2023-12-02 15:12:36 +0100

    combined gitignore

--- client/src/components/MensajesList.tsx
+++ client/src/components/MensajesList.tsx
@@ -11,7 +11,7 @@ const MensajesList = ({ mensajesFiltrados }: MensajesListProps): JSX.Element => 
       {mensajesFiltrados.map((mensaje) => (
         <div key={mensaje.id} className='text-white flex flex-col mt-3'>
           <p className='border border-purple-200 w-min rounded-xl ml-1 opacity-80 text-sm'>@{mensaje.usuario}</p>
-          <p className='border border-purple-400 rounded-2xl text-lg pl-2 pr-2'>{mensaje.contenidoMensaje}</p>
+          <p className='border border-purple-400 w-fit rounded-2xl text-lg pl-2 pr-2'>{mensaje.contenidoMensaje}</p>
         </div>
       ))}
     </section>
